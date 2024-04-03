c04b4fe4b7229add2c96328678b4d67b1926158f
PedroSawczuk <pedrosawczuk53@gmail.com>
2024-04-03 17:17:50 -0400

    Urls, Views e Admin

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import include, path
+from .views import *
+
+urlpatterns = [
+    path('', HomeView.as_view(), name="home"),
+    path('categorias', CategoriaListView.as_view(), name="categorias"),
+    path('<slug:slug>/produtos', ProdutosListView.as_view(), name="listarprodutos")
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import *
+from app.models import *
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+class CategoriaListView(ListView):
+    template_name = 'produtos/categorias.html'
+    context_object_name = 'categorias'  
+    model = Categoria   
+
+class ProdutosListView(ListView):
+    model = Produto
+    template_name = 'produtos/listarprodutos.html'
+    context_object_name = 'produtos'
+    queryset = Produto.disponiveis.all()
+    
+    def get_queryset(self, slugcat=None):
+        qs = super().get_queryset()
+        if slugcat:
+            cat = Categoria.get(slug=slugcat)
+            qs = qs.filter(categoria=cat)
+        return qs
+    
+    def get_context_data(self, slugcat=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if slugcat:
+            context['categoria'] = Categoria.objects.get(slug=slugcat)
+        return context
